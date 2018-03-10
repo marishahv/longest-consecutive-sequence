ecdc52be105dabe8396ea70448a25ea436f34fc8
Marina <35458264+marishahv@users.noreply.github.com>
2018-03-10 13:38:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    var len = array.length;  
+    var maxCount = 0; tempCount = 1;  
+      
+    array.sort((a, b) => a - b);
+
+    for(var i = 0; i < len; i++)
+    {
+        if((array[i + 1] - array[i]) == 1){
+            tempCount++;
+        } else {
+            if((array[i + 1] - array[i]) == 0)
+                continue;
+            if(tempCount > maxCount){
+                maxCount = tempCount;
+            }
+            tempCount = 1;
+        }
+    }
+    return maxCount;
 }
